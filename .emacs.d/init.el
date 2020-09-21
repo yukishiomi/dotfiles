694f9d074f43512adbed7bc2e9cf57dd66412d3f
Yuki Shiomi <luckey.ss0209@gmail.com>
2020-09-21 22:08:11 +0900

    修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,6 +9,26 @@
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
             (normal-top-level-add-subdirs-to-load-path))))))
 
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "https://marmaladerepo.org/packages/"))
 (add-to-list 'package-archives '("melpa"."https://melpa.org/packages/"))
@@ -135,6 +155,21 @@
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)))
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "jsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; configure jsx-tide checker to run after your default jsx checker
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
 ;; gtags-modeのキーバインドを有効化				  
 (setq gtags-suggested-key-mapping t)
 ;; ファイル保存人い自動的にタグをアップデート
@@ -142,8 +177,7 @@
 (require 'ctags nil t)
 (setq tags-revert-without-query t)
 (setq ctags-command "ctags -R --fields=\"+afikKlmnsSzt\" ")
-(global-set-key (kbd "<f5>") 'ctags-create-or-update-tags-table)
-(global-set-key (kbd "M-.") 'ctags-search)
+
 
 (setq rinari-tags-file-name "TAGS")
 
